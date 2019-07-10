4b664f44801c2ccd3a1f27f23df90399e0f66ea6
vladmog <vladmog@gmail.com>
2019-07-10 14:47:35 -0700

    Added ability to change date from a form in app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import Date from './Date';
 import FetchDate from './FetchDate';
 
 function App() {
-  const [fetchDate, setFetchDate] = useState()
+  const [fetchDate, setFetchDate] = useState(
+    {month: null,
+    day: null,
+    year: null,
+    dateGiven: false}
+  )
   const [data, setData] = useState(
       {copyright: '',
       date: '',
@@ -19,19 +24,29 @@ function App() {
       url: '',
     });
 
+
+
   const fetchData = () => {
-    axios.get ('https://api.nasa.gov/planetary/apod?api_key=wiOcfSgtOXuVwPMP0rKNbhnhcTC1H58IRNv4N0Oe&date=2012-03-14')
+    if (fetchDate.dateGiven){
+      axios.get (`https://api.nasa.gov/planetary/apod?api_key=wiOcfSgtOXuVwPMP0rKNbhnhcTC1H58IRNv4N0Oe&date=${fetchDate.year}-${fetchDate.month}-${fetchDate.day}`)
       .then(res => {
         setData(res.data)
       })
       .catch(err => {console.log(err)})
+    }else{
+      axios.get (`https://api.nasa.gov/planetary/apod?api_key=wiOcfSgtOXuVwPMP0rKNbhnhcTC1H58IRNv4N0Oe&date=2012-03-14`)
+        .then(res => {
+          setData(res.data)
+        })
+        .catch(err => {console.log(err)})
+      }
   }
 
   useEffect(() => {
     fetchData();
   }, [fetchDate])
 
-  console.log("data:",data)
+  
   return (
     <div className="App">
       <FetchDate setFetchDate = {setFetchDate} />
--- src/FetchDate.js
+++ src/FetchDate.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Component} from 'react';
 
 class FetchDate extends Component {
@@ -16,30 +16,43 @@ class FetchDate extends Component {
 
     submitDate = e => {
         e.preventDefault();
+        let padMonth = this.state.month.padStart(2, '0')
+        let padDay = this.state.day.padStart(2, '0')
+        let padYear = this.state.year.padStart(4, '0')
+
+        this.props.setFetchDate(
+            {month: padMonth, 
+            day: padDay, 
+            year: padYear,
+            dateGiven: true})
     }
 
     render(){
         return (
             <div>
-                <form>
+                <form onSubmit = {this.submitDate}>
                     <input 
                         type = "number"
+                        name = "month"
                         placeholder = "MM"
                         value = {this.state.month}
                         onChange = {this.changeHandler}
                     />
                     <input 
                         type = "number"
+                        name = "day"
                         placeholder = "DD"
-                        value = {this.state.month}
+                        value = {this.state.day}
                         onChange = {this.changeHandler}
                     />
                     <input 
                         type = "number"
+                        name = "year"
                         placeholder = "YYYY"
-                        value = {this.state.month}
+                        value = {this.state.year}
                         onChange = {this.changeHandler}
                     />
+                    <button onClick = {this.submitDate}>SUBMIT</button>
                 </form>
             </div>
         )
