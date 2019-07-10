20dfbd766952352dfd5c384d671f2bf2d40c420e
vladmog <vladmog@gmail.com>
2019-07-10 15:34:54 -0700

    Added increment buttons to change date

--- src/App.js
+++ src/App.js
@@ -23,35 +23,86 @@ function App() {
       title: '',
       url: '',
     });
-
+  const [incrementedDate, setIncrementedDate] = useState(
+    {
+      month: null,
+      day: null,
+      year: null,
+      dateIncremented: false
+    }
+  );
 
 
   const fetchData = () => {
-    if (fetchDate.dateGiven){
+    if (incrementedDate.dateIncremented){
+      console.log("incrementedDate")
+      axios.get (`https://api.nasa.gov/planetary/apod?api_key=wiOcfSgtOXuVwPMP0rKNbhnhcTC1H58IRNv4N0Oe&date=${incrementedDate.year}-${incrementedDate.month}-${incrementedDate.day}`)
+      .then(res => {
+        console.log("NEW AXIOS CALL DATE", res.data.date)
+        setData(res.data)
+      })
+      .catch(err => {console.log(err)})
+    }else if (fetchDate.dateGiven){
       axios.get (`https://api.nasa.gov/planetary/apod?api_key=wiOcfSgtOXuVwPMP0rKNbhnhcTC1H58IRNv4N0Oe&date=${fetchDate.year}-${fetchDate.month}-${fetchDate.day}`)
       .then(res => {
+        console.log(res.data.date)
         setData(res.data)
       })
       .catch(err => {console.log(err)})
     }else{
       axios.get (`https://api.nasa.gov/planetary/apod?api_key=wiOcfSgtOXuVwPMP0rKNbhnhcTC1H58IRNv4N0Oe&date=2012-03-14`)
         .then(res => {
+          console.log(res.data.date)
           setData(res.data)
         })
-        .catch(err => {console.log(err)})
+        .catch(err => {})
       }
   }
 
   useEffect(() => {
     fetchData();
-  }, [fetchDate])
+  }, [fetchDate, incrementedDate])
 
+
+  const incrementDate = (direction) => {
+    console.log("Current date:",data.date)
+    var dateSplit = data.date.split("-");
+    //=====================
+    let dd = dateSplit[2];
+    let mm = dateSplit[1];
+    let yyyy = dateSplit[0];
+
+    console.log("SPLIT CURRENT DATE", dd, mm, yyyy)
+    
+    if (direction === "next"){
+      console.log("NEXT TRIGGER")
+      setIncrementedDate(
+        {month: mm,
+        day: parseFloat(dd) + 1,
+        year: yyyy,
+        dateIncremented: true}
+      )
+    }else if (direction === "prev"){
+      console.log("PREV TRIGGER")
+      setIncrementedDate(
+        {month: mm,
+        day: dd - 1,
+        year: yyyy,
+        dateIncremented: true}
+      )
+    }
+  }
   
   return (
     <div className="App">
+      
       <FetchDate setFetchDate = {setFetchDate} />
       <Image image = {data.url}/>
-      <Date date = {data.date} />
+      <div className = "dateNav">
+        <button onClick = {() => incrementDate("prev")}>Prev</button>
+        <Date date = {data.date} />
+        <button onClick = {() => incrementDate("next")}>Next</button>
+      </div>
       <Explanation explanation = {data.explanation} />
     </div>
   );
